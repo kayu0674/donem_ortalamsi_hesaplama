a82d2b55f5c2120f0c16c945d5d2c341be9922b4
kayu <gulecyusuf0674@gmail.com>
2025-12-23 16:11:57 +0300

    her dersin vize final quiz not girisi kodları yazıldı

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("inkıkap vize notunu gir");
+float inkv = float.Parse(Console.ReadLine());
+Console.WriteLine("inkıkap final notunu gir");
+float inkf = float.Parse(Console.ReadLine());
+
+Console.WriteLine("turkdili vize notunu gir");
+float trv = float.Parse(Console.ReadLine());
+Console.WriteLine("turkdili final notunu gir");
+float trf = float.Parse(Console.ReadLine());
+
+Console.WriteLine("ingilizce vize notunu gir");
+float ingv = float.Parse(Console.ReadLine());
+Console.WriteLine("ingilizce final notunu gir");
+float ingf = float.Parse(Console.ReadLine());
+
+Console.WriteLine("siber guvenlik vize notunu gir");
+float sbrv = float.Parse(Console.ReadLine());
+Console.WriteLine("siber guvenlik final notunu gir");
+float sbrf = float.Parse(Console.ReadLine());
+
+Console.WriteLine("yapayzeka vize notunu gir");
+float yzv = float.Parse(Console.ReadLine());
+Console.WriteLine("yapayzeka final notunu gir");
+float yzf = float.Parse(Console.ReadLine());
+Console.WriteLine("yapayzeka quiz ortalamanı gir");
+float yzq = float.Parse(Console.ReadLine());
+
+Console.WriteLine("bil donanımı vize notunu gir");
+float donv = float.Parse(Console.ReadLine());
+Console.WriteLine("bil donanımı final notunu gir");
+float donf = float.Parse(Console.ReadLine());
+Console.WriteLine("bil donanımı 1.quiz notunu gir");
+float donq1 = float.Parse(Console.ReadLine());
+Console.WriteLine("bil donanımı 2.quiz notunu gir");
+float donq2 = float.Parse(Console.ReadLine());
+Console.WriteLine("bil donanımı 3.quiz notunu gir");
+float donq3 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("matematik vize notunu gir");
+float matv = float.Parse(Console.ReadLine());
+Console.WriteLine("matematik final notunu gir");
+float matf = float.Parse(Console.ReadLine());
+
+Console.WriteLine("veritabanı vize notunu gir");
+float veriv = float.Parse(Console.ReadLine());
+Console.WriteLine("veritabanı final notunu gir");
+float verif = float.Parse(Console.ReadLine());
+Console.WriteLine("veritabanı quiz notunu gir");
+float veriq = float.Parse(Console.ReadLine());
+
+Console.WriteLine("programlama vize notunu gir");
+float progv = float.Parse(Console.ReadLine());
+Console.WriteLine("programlama final notunu gir");
+float progf = float.Parse(Console.ReadLine());
